2cb02b18da70eae0e59b043cfa7cbdd351eb125f
Nina van Boxtel <nina.van.boxtel@mollie.com>
2023-04-30 20:56:36 +0000

    fixed typo

--- greenery/models/marts/product/intermediate/int_product_events.sql
+++ greenery/models/marts/product/intermediate/int_product_events.sql
@@ -11,7 +11,7 @@ WITH events AS (
 SELECT 
  product_id
 , DATE(created_at) AS created_day
-, SUM(CASE WHEN event_type = 'add_to_cart' THEN 1 ELSE 0 END) AS add_to_cart
-, SUM(CASE WHEN event_type = 'page_view' THEN 1 ELSE 0 END) AS page_view
+, SUM(CASE WHEN event_type = 'add_to_cart' THEN 1 ELSE 0 END) AS add_to_carts
+, SUM(CASE WHEN event_type = 'page_view' THEN 1 ELSE 0 END) AS page_views
 FROM events
 GROUP BY 1,2
